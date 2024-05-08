d94db171bccdc67558c480e33df06021f9a95d4f
KhEsanukaev <halid.esanukaev.86@mail.ru>
2024-05-08 17:44:55 +0300

    доработал адаптацию

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -30,37 +30,40 @@ const Header: React.FC<HeaderProps> = ({ searchQuery, setSearchQuery }) => {
           </Link>
         </div>
 
-        <ul className={styles.cont_ul}>
-          <li>
-            <Link to={`/`} className={styles.mainLink}>
-              ГЛАВНАЯ
-            </Link>
-          </li>
-          <li>
-            <Link to={"/aboutus"} className={styles.aboutLink}>
-              О НАС
-            </Link>
-          </li>
-          <li>
-            <Link to={"/contacts"} className={styles.contactLink}>
-              КОНТАКТЫ
-            </Link>
-          </li>
-          <li>
-            <Link to={"/upload"} className={styles.uploadLink}>
-              ДЛЯ СКАЧИВАНИЯ
-            </Link>
-          </li>
-        </ul>
-        <div>
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={handleChange}
-            placeholder="Поиск продукции..."
-            className={styles.inp}
-          />
+        <div className={styles.nav_menu}>
+          <ul className={styles.cont_ul}>
+            <li>
+              <Link to={`/`} className={styles.mainLink}>
+                ГЛАВНАЯ
+              </Link>
+            </li>
+            <li>
+              <Link to={"/aboutus"} className={styles.aboutLink}>
+                О НАС
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contacts"} className={styles.contactLink}>
+                КОНТАКТЫ
+              </Link>
+            </li>
+            <li>
+              <Link to={"/upload"} className={styles.uploadLink}>
+                ДЛЯ СКАЧИВАНИЯ
+              </Link>
+            </li>
+          </ul>
+          <div>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleChange}
+              placeholder="Поиск продукции..."
+              className={styles.inp}
+            />
+          </div>
         </div>
+
         <div className={styles.call}>
           <span>+79899309981</span>
           <span>goldtrader95@mail.ru</span>
