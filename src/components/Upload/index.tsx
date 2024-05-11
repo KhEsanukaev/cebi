4c00f453251b29a7c0abf647361029caa53bf2a9
KhEsanukaev <halid.esanukaev.86@mail.ru>
2024-05-11 20:09:39 +0300

    сделала адаптацию upload

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import cebilogo from '../../img/cebilogo.jpg';
-import styles from '../Upload/upload.module.css';
-import Contacts from '../Contacts';
+import React from "react";
+import cebilogo from "../../img/cebilogo.jpg";
+import styles from "../Upload/upload.module.css";
+import Contacts from "../Contacts";
+import workUs from "../../img/workUs.jpg";
 
 const Upload: React.FC = () => {
   return (
     <div className={styles.upload}>
-      <div>
-        <div>
-          <h2 className={styles.h2_catalog}>Каталог</h2>
+      <div className={styles.catalog}>
+        <div className={styles.catalog2}>
+          <div>
+            <h2 className={styles.h2_catalog}>Каталог</h2>
+          </div>
+          <div>
+            <img src={cebilogo} alt="Логотип" width={200} height={200} />
+          </div>
+          <div className={styles.link}>
+            <h2>
+              <a href="https://drive.google.com/file/d/1wsIGHuZoMzRwsI9EynMmOKLra-8GRSaw/view">
+                Скачать каталог дверных ручек
+              </a>
+            </h2>
+          </div>
+          <div className={styles.link2}>
+            <h2>
+              <a href="https://drive.google.com/file/d/1z-0UdRm8HhVsil6UMljk0kMZnQZtEWqu/view">
+                Инструкция по установке дверных ручек
+              </a>
+            </h2>
+          </div>
         </div>
-        <div>
-          <img src={cebilogo} alt="Логотип" width={200} height={200} />
-        </div>
-        <div className={styles.link}>
-          <h2>
-            <a href="https://drive.google.com/file/d/1wsIGHuZoMzRwsI9EynMmOKLra-8GRSaw/view">
-              Скачать каталог дверных ручек
-            </a>
-          </h2>
-        </div>
-        <div className={styles.link2}>
-          <h2>
-            <a href="https://drive.google.com/file/d/1z-0UdRm8HhVsil6UMljk0kMZnQZtEWqu/view">
-              Инструкция по установке дверных ручек
-            </a>
-          </h2>
+
+        <div className={styles.workUse}>
+          <img src={workUs} alt="" width={1082} height={420}/>
         </div>
       </div>
       <Contacts showMap={false} />
